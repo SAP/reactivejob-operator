201dc65d5471d57c7e6588a8187634f4ca0e4ccd
I312404 <ragunath.krishnaraju@sap.com>
2024-11-28 18:40:33 +0530

    chore: update SPDX copyright year to 2024

--- pkg/client/clientset/versioned/typed/batch.cs.sap.com/v1alpha1/batch.cs.sap.com_client.go
+++ pkg/client/clientset/versioned/typed/batch.cs.sap.com/v1alpha1/batch.cs.sap.com_client.go
@@ -1,5 +1,5 @@
 /*
-SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and reactivejob-operator contributors
+SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and reactivejob-operator contributors
 SPDX-License-Identifier: Apache-2.0
 */
 
